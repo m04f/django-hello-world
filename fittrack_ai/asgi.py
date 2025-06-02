c509fa4658b9ba16211823400fac7dcef317a36a
Mostafa Khaled <m.khaled.dev@gmail.com>
2025-06-02 18:29:41 +0300

    fix asgi server startup

--- fittrack_ai/asgi.py
+++ fittrack_ai/asgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
-import chat.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fittrack_ai.settings')
+django.setup()
+
+import chat.routing
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -27,3 +29,5 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
+app = application
